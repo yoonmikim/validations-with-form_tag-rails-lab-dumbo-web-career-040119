3449765bd5c03d427f4c7562113d7063ae1dacb6
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 18:55:44 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,10 @@ class AuthorsController < ApplicationController
   
   before_action :get_author, only: [:show, :edit, :update]
   
+  def index 
+    @authors = Author.all
+  end
+  
   def show
     @author = Author.find(params[:id])
   end
@@ -16,6 +20,20 @@ class AuthorsController < ApplicationController
       redirect_to author_path(@author)
     else 
       render :new
+    end
+  end
+  
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.valid?
+      @author.update(author_params)
+      redirect_to author_path(@author)
+    else 
+      render :edit
     end
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
   before_action :get_post, only: [:show, :edit, :update]
   
+  def index 
+    @posts = Post.all
+  end
+  
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid? 
+      redirect_to post_path(@post)
+    else 
+      render :new
+    end
+  end
+
 
   def edit
     @post = Post.find(params[:id])
