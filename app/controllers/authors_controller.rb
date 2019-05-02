a26905c4b069813f94695d361b298497907c4d59
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 19:05:59 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,11 @@
 class AuthorsController < ApplicationController
+  
+  before_action :get_author, only: [:show, :edit, :update]
+  
+  def index 
+    @authors = Author.all
+  end
+  
   def show
     @author = Author.find(params[:id])
   end
@@ -8,13 +15,34 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid? 
+      redirect_to author_path(@author)
+    else 
+      render :new
+    end
+  end
+  
+  def edit
+    @author = Author.find(params[:id])
+  end
 
-    redirect_to author_path(@author)
+  def update
+    @author = Author.find(params[:id])
+    if @author.valid?
+      @author.update(author_params)
+      redirect_to author_path(@author)
+    else 
+      render :edit
+    end
   end
 
   private
 
+  def get_author 
+    @author = Author.find(params[:id])
+  end 
+  
   def author_params
     params.permit(:name, :email, :phone_number)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,27 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show, :edit, :update]
+  
+  def index 
+    @posts = Post.all
+  end
+  
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid? 
+      redirect_to post_path(@post)
+    else 
+      render :new
+    end
+  end
+
 
   def edit
     @post = Post.find(params[:id])
@@ -9,13 +29,19 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else 
+      render :edit
+    end
   end
 
   private
+  
+  def get_post 
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:title, :category, :content)
